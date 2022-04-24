aedbbba66a943ca9abd71c506b791d68fe39073a
Fyam <fung.yam@gtomato.com>
2022-04-24 22:37:26 +0800

    Update Root

--- common/src/commonMain/kotlin/com/fyam/flowTest/Root.kt
+++ common/src/commonMain/kotlin/com/fyam/flowTest/Root.kt
@@ -1,11 +1,41 @@
 package com.fyam.flowTest
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.fyam.flowTest.components.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlin.time.DurationUnit
 
 @Composable
 fun Root(
     modifier: Modifier = Modifier,
 ) {
+    val logger = rememberLoggerState()
+    LazyColumn(modifier) {
+        item {
+            Counter()
+        }
+        item {
+            FlowRow(
+                content = {
+                    repeat(10) {
 
+                        JobButton(
+                            title = "Button-$it",
+                            onDone = { time ->
+                                logger log time.toString(DurationUnit.MILLISECONDS)
+                            },
+                            onClick = {
+                            }
+                        )
+                    }
+                }
+            )
+        }
+        item {
+            Logger(state = logger)
+        }
+    }
 }
